c90e9a322b5ee0146aa735c1121dbb4261e081cc
Dan Absher <Dan.Absher@marcusmillichap.com>
2025-10-31 07:59:46 -0700

    Remove log statement

--- src/s3-api/in-memory-storage.ts
+++ src/s3-api/in-memory-storage.ts
@@ -2,8 +2,6 @@ import { Customer } from "../storage/customer";
 
 const store: Customer[] = [];
 
-console.log('creating store');
-
 export class InMemoryStorage {
     async saveCustomer(customer: Customer): Promise<string> {
         customer.Id = (store.length + 1).toString();
